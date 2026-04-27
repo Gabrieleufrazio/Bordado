c6f0cf67dbaeb2aba39f8fb8c34ce3b78a891191
Gabrieleufrazio <Gabrieleufrazio@users.noreply.github.com>
2026-04-27 03:46:58 +0000

    deploy: baa403a2ce47378bb79fc8a17fcd6c7ffd1892c5

--- server/assets/_tanstack-start-manifest_v-B8Bkg3aW.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/runner/work/Bordado/Bordado/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/Bordado/assets/index-DC3CowMB.js"] }, "/": { filePath: "/home/runner/work/Bordado/Bordado/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/Bordado/assets/index-DSwpPhRS.js"] } }, clientEntry: "/Bordado/assets/index-DC3CowMB.js" });
-export {
-  tsrStartManifest
-};
